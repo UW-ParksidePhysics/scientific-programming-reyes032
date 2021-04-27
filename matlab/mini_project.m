486fe35b0e0b8f187ddf2d75772c768370fddabe
williamparker <baiweilian@gmail.com>
2021-04-27 09:03:35 -0500

    Added reviewer comments to mini-project template

--- matlab/mini_project.m
+++ matlab/mini_project.m
@@ -17,3 +17,29 @@
 % Function definitions for simulation solution & visualization
 %	Each function contains help text: https://www.mathworks.com/help/matlab/matlab_prog/add-help-for-your-program.html
 
+%%%% Feedback from reviewer
+%%1. The code has a few errors that I noticed.
+    %line 17 "reactance" doesnt seem to be mentioned any where else in the
+    %code  so im not sure if that was a typo or if they forgot to define
+    %what "reactance" is before imputting it into the equation.
+    
+    %line 18 seems to be missing a closing parentheses
+    
+    %the term voltage seems to be mentioned alot, im not sure what value or
+    %equation is meant to be assigned to that term
+%2. Since the code has a couple errors i cant see the output of the code.
+    %Knowing this is someone else's code I dont feel comfortable editing it
+    %to try to get it to work.
+
+%3. The code is pretty readable. Since everything is spelled out it just
+%takes me a little longer to process the variables in my head as to what
+%they represent etc. I'm used to seeing equations represent via single
+%letters.
+
+%4. There arent very many comments, which isnt a bad thing entirely.
+%5. I think the variable names relate exactly to what they represent, since
+%everything is spelled out theres little to no room for interpretation.
+%6. I'm not sure what problem they are trying to solve. From what I can
+%gather they are working through a circuit with a preset voltage trying to
+%determine the resistance, current, and total power of the circuit.
+%7. There are no visualizations for this code atm.
