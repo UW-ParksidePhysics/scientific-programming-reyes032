44e1d9f89c4048ba3ecf5f7d10e4bdd2c37b80c8
Cheo's pc <reyes032@rangers.uwp.edu>
2021-04-28 16:46:49 -0500

    birthday

--- python/ball.py
+++ python/ball.py
@@ -1,3 +1,3 @@
-import vpython as vp
 
-vp.sphere()
+
+
--- python/two_balls_bounce.py
+++ python/two_balls_bounce.py
@@ -1,20 +1,23 @@
 import vpython as vp
 
-initial_position = vp.vector(-10., 0., 0.)
-initial_velocity = vp.vector(25., 0., 0.)
+initial_position = vp.vector(-10., -5., 0.)
+initial_position_2 = vp.vector(-10., 5., 0.)
+initial_velocity = vp.vector(20., 10., 0.)
+initial_velocity_2 = vp.vector(20., -10., 0.)
 ball = vp.sphere(pos=initial_position, radius=0.5, color=vp.color.blue, make_trail=True)
-
+ball_2 = vp.sphere(pos=initial_position_2, radius=0.5, color=vp.color.green, make_trail=True)
 wall_center = vp.vector(0., 0., 0.)
 wall_dimensions = vp.vector(0.25, 10., 10.)
 wall = vp.box(pos=wall_center, size=wall_dimensions, color=vp.color.red)
 
 animation_time_step = 0.01  # seconds
-rate_of_animation = 1/animation_time_step
+rate_of_animation = 1 / animation_time_step
 time_step = 0.005
 stop_time = 1.
 
 time = 0.
 ball_velocity = initial_velocity
+ball_2_velocity = initial_velocity_2
 while time < stop_time:
     vp.rate(rate_of_animation)
     if ball.pos.x > wall.pos.x:
@@ -22,4 +25,9 @@ while time < stop_time:
     ball.pos.x += ball_velocity.x * time_step
     ball.pos.y += ball_velocity.y * time_step
     ball.pos.z += ball_velocity.z * time_step
+    if ball_2.pos.x > wall.pos.x:
+        ball_2_velocity.x = -ball_2_velocity.x  # reverse ball_2 velocity
+    ball_2.pos.x += ball_2_velocity.x * time_step
+    ball_2.pos.y += ball_2_velocity.y * time_step
+    ball_2.pos.z += ball_2_velocity.z * time_step
     time += time_step
