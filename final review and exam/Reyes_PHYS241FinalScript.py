381829d55aa44a35a144f39e830e6c2e37a96331
williamparker <baiweilian@gmail.com>
2021-05-13 14:56:31 -0500

    Added fit parameters return from fit_eos and labeled in main script

--- final review and exam/Reyes_PHYS241FinalScript.py
+++ final review and exam/Reyes_PHYS241FinalScript.py
@@ -26,18 +26,18 @@ stats = bivariate_statistics(array)
 quadratic_coefficients = quadratic_fit(array)
 volumes = array[0, :]
 energies = array[1, :]
-fit_eos_array = fit_eos(volumes, energies, quadratic_coefficients, eos="vinet", number_of_points=50)
-#print(fit_eos_array)
+fit_eos_array, fit_parameters = fit_eos(volumes, energies, quadratic_coefficients, eos="murnaghan", number_of_points=50)
 
+bulk_modulus = fit_parameters[1]
+equilibrium_volume = fit_parameters[3]
+
+# Define an array of fit volumes going from volumes[0] to volumes[-1] but equal in number of points to fit_eos_array
 
 plt.plot(volumes, energies, 'bo')
-plt.plot(fit_eos_array, 'k')
+plt.plot(fit_volumes, fit_eos_array, 'k')
 plt.xlabel(r'$\mathit{V}(\AA^3$/atom)')
 plt.ylabel(r'$\mathit{E}$ (eV/atom)')
 
 
 plt.show()
-
-
-
 
