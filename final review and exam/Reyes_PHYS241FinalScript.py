ee3b69c7c65c0b2ba6309723626fc0cb0ff55462
Cheo's pc <reyes032@rangers.uwp.edu>
2021-05-13 20:50:49 -0500

    a million resse's cups later

--- final review and exam/Reyes_PHYS241FinalScript.py
+++ final review and exam/Reyes_PHYS241FinalScript.py
@@ -4,7 +4,7 @@ from two_column_text_read import two_column_text_read
 from byvariate_statistics import bivariate_statistics
 from quadratic_fit import quadratic_fit
 import matplotlib.pyplot as plt
-from plot_data_with_fit import plot_data_with_fit
+
 
 
 
@@ -27,17 +27,26 @@ quadratic_coefficients = quadratic_fit(array)
 volumes = array[0, :]
 energies = array[1, :]
 fit_eos_array, fit_parameters = fit_eos(volumes, energies, quadratic_coefficients, eos="murnaghan", number_of_points=50)
-
+fit_volumes = np.linspace(volumes[0], volumes[-1], 50)
 bulk_modulus = fit_parameters[1]
 equilibrium_volume = fit_parameters[3]
-
+print(equilibrium_volume)
 # Define an array of fit volumes going from volumes[0] to volumes[-1] but equal in number of points to fit_eos_array
 
 plt.plot(volumes, energies, 'bo')
 plt.plot(fit_volumes, fit_eos_array, 'k')
 plt.xlabel(r'$\mathit{V}(\AA^3$/atom)')
 plt.ylabel(r'$\mathit{E}$ (eV/atom)')
-
-
+plt.title('Murnaghan Equation of State for Ag in DFT GGA-PBE')
+plt.text(100.79, -287.32865, 'Created by Cheo Reyes 2020-05-12')
+plt.text(120.94, -287.32250,'$\mathit{V}=120.549(\AA^3$/atom)')
+plt.text(100.73, -287.31470, "Ag")
+plt.text(115.19, -287.32454,'$\mathit{Fm}3\mathit{m}$')
+plt.text(115.50, -287.32360, '$\mathit{K}$0 = .006116 Gpa ')
+plt.axvline(120.549, color = 'k', linestyle = '--')
 plt.show()
 
+if display_graph == True:
+    plt.show()
+else:
+    plt.savefig()
